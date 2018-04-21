9ffc0cc43b3787d7a958dc4294baf92f792aeca2
Cody Romano <codyromano@gmail.com>
2018-04-20 20:55:26 -0700

    getGameObjectsInPosition util method

--- src/store/gameDataUtils.js
+++ src/store/gameDataUtils.js
@@ -1,1 +1,8 @@
 export const serializeGameObjectLocation = (x, y) => [x, y].join(',');
+
+export const getGameObjectsInPosition = (reduxState, coords) => {
+  const locationKey = serializeGameObjectLocation(coords);
+  const resourceIds = reduxState.resources.byPosition[locationKey] || [];
+
+  return resourceIds.map(id => reduxState.resources.byId[id]);
+};
