09b7e66ce8c3448b4fba84c61f1a761fa6bad0a5
Cody Romano <codyromano@gmail.com>
2018-04-21 18:11:04 -0700

    SoundEffect container

---
+++ src/components/data-providers/WrappedWithSound/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+* A container that plays a sound effect when the user interacts
+* with any of its child components.
+*/
+export class SoundEffect extends React.Component {
+  static propTypes = {
+    listenerType: PropTypes.oneOf(['onClick', 'onMouseDown']),
+    onError: PropTypes.func
+  };
+  static defaultProps = {
+    listenerType: 'onClick',
+    onError: console.error.bind(console)
+  };
+  constructor(props) {
+    super(props);
+    this.playSound = this.playSound.bind(this);
+  }
+  playSound() {
+    try {
+      const audio = document.createElement('audio');
+      audio.src = this.props.audioUrl;
+      audio.play();
+    } catch (error) {
+      this.props.onError(error);
+    }
+  }
+  render() {
+    const { listenerType, children } = this.props;
+    /* An alternative would be rendering <audio>. Though this is more declarative,
+    it requires adding an additional wrapper in the DOM, and playback will stop
+    when SoundEffect unmounts. It's a trade-off... */
+    return React.Children.map(
+      children,
+      child => React.cloneElement(child, {
+        [listenerType]: this.playSound
+      })
+    );
+  }
+}
+
+export const withSound = (soundOptions = {}) => (Component) => (props) => (
+  <SoundEffect {...soundOptions}>
+    <Component {...this.props} />
+  </SoundEffect>
+);
