463048e1494145204ab971feb13c0849698c4a2a
Cody Romano <codyromano@gmail.com>
2018-04-22 17:41:46 -0700

    Redesign Place Tile page; create meaningful plant item descriptions

--- src/components/pages/PlaceTileResource/index.js
+++ src/components/pages/PlaceTileResource/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { plantResourceAcquired } from '../../../store/actions';
-import ThumbnailGridMenu from '../../ThumbnailGridMenu';
+import { plantResourceAcquired } from 'aurae-actions';
 import BasePage from '../BasePage';
 import { routePaths } from '../../../routes';
 import { withRouter } from 'react-router';
 
 import { PageWidthContainer } from '../../layout';
-import { TAB_ID_GARDEN } from '../../../config/tabsMenuConfig';
+import { TAB_ID_GARDEN } from 'aurae-config/tabsMenuConfig';
+import Button from 'material-ui/Button';
+import Card, { CardContent, CardActions, CardHeader } from 'material-ui/Card';
+import Avatar from 'material-ui/Avatar';
 
 class PlaceTileResource extends React.Component {
   constructor(props) {
@@ -20,21 +22,52 @@ class PlaceTileResource extends React.Component {
     this.props.plantResourceAcquired(resource, this.props.selectedCoords);
     this.props.history.push(routePaths.VIEW_GARDEN);
   }
-  render() {
-    return (
+  render = () => (
       <BasePage selectedTabId={TAB_ID_GARDEN}>
         <PageWidthContainer>
-          <h1>Choose a plant</h1>
-          <ThumbnailGridMenu
-            onSelect={this.onSelect}
-          />
+          <h1>Plant an item</h1>
+
+            {this.props.gameItems.map(item => {
+              const cardHeaderProps = {
+                avatar: <Avatar src={item.imageSrc} />,
+                title: <span>{item.title}</span>,
+                subheader: <span>{item.itemClassDescription}</span>
+              };
+              const cardContentProps = {
+                // TODO: Inline style is a hack
+                component: () => <div style={{padding: '0 1.5rem'}}>
+                  <p style={{paddingTop: 0}}>{item.fullDescription}</p>
+                </div>
+              };
+
+              const cardStyles = {
+                margin: '1rem 0'
+              };
+
+              return (
+                <Card key={item.title} style={cardStyles}>
+                  <CardHeader {...cardHeaderProps} />
+                  <CardContent {...cardContentProps} />
+                  <CardActions style={{alignItems: 'right'}}>
+                    <Button
+                      onClick={() => this.onSelect(item)}
+                      color="primary">Choose plant</Button>
+                  </CardActions>
+                </Card>
+              );
+            })}
         </PageWidthContainer>
       </BasePage>
     );
-  }
 }
 
+const itemShape = PropTypes.shape({
+  title: PropTypes.string.isRequired,
+  imageSrc: PropTypes.string.isRequired
+}).isRequired;
+
 PlaceTileResource.propTypes = {
+  gameItems: PropTypes.arrayOf(itemShape),
   selectedCoords: PropTypes.arrayOf(
     PropTypes.number
   ).isRequired,
@@ -46,8 +79,10 @@ PlaceTileResource.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  selectedCoords: state.currentTile.coords
+  selectedCoords: state.currentTile.coords,
+  gameItems: state.gameItems
 });
+
 const mapDispatchToProps = (dispatch) => bindActionCreators({
   plantResourceAcquired
 }, dispatch);
--- src/store/reducers/gameItemsReducer.js
+++ src/store/reducers/gameItemsReducer.js
@@ -1,26 +1,20 @@
 const initialState = [
   {
-    title: 'Sakura Tree',
-    imageSrc: 'https://s3-us-west-2.amazonaws.com/codyromano/project-aurae/Sakura.jpg',
-    description: 'Description',
+    title: 'Sweet Sprout',
+    itemClassDescription: 'Dessert plant',
+    fullDescription: `Discovered by Belgian explorers in the year 2035, the Sweet Sprout
+      is a mystical cold-weather plant. With enough water, it evolves into the Sugar
+      Oak: a mighty tree that yields baked delicacies.`,
+    imageSrc: 'https://s3-us-west-2.amazonaws.com/codyromano/project-aurae/plant-image-sugar-sprout.jpg',
     stats: {}
   },
   {
-    title: 'Rose',
-    imageSrc: '	https://s3-us-west-2.amazonaws.com/codyromano/project-aurae/Rose.jpg',
-    description: 'Description',
-    stats: {}
-  },
-  {
-    title: 'Alaska Cedar Sprout',
-    imageSrc: 'https://s3-us-west-2.amazonaws.com/codyromano/project-aurae/cedar.jpg',
-    description: 'Description',
-    stats: {}
-  },
-  {
-    title: 'Blackfruit Dogwood',
-    imageSrc: 'https://s3-us-west-2.amazonaws.com/codyromano/project-aurae/Blackfruit_Dogwood.jpg',
-    description: 'Description',
+    title: 'Steamy Java Bean',
+    itemClassDescription: 'Coffee plant',
+    fullDescription: `A marvel of genetic engineering, the Steamy Java Bean contains billions
+      of nanorobots. The bots arrange themselves dinto elaborate cappuccino art while your
+      neighborhood barista is pouring you a cup.`,
+    imageSrc: 'https://s3-us-west-2.amazonaws.com/codyromano/project-aurae/plant-image-sugar-java-bean.jpg',
     stats: {}
   }
 ];
