c1594b7749bddf677709c27201978091da1d05f0
Cody Romano <codyromano@gmail.com>
2018-04-24 20:37:15 -0700

    Add aurae-routes

--- config/webpack.alias.js
+++ config/webpack.alias.js
@@ -7,7 +7,8 @@ const aliases = {
   'aurae-components': 'src/components',
   'aurae-pages': 'src/components/pages',
   'aurae-common-shapes': 'src/components/commonShapes',
-  'aurae-resource-classes': 'src/config/resourceClasses'
+  'aurae-resource-classes': 'src/config/resourceClasses',
+  'aurae-routes': 'src/routes.js'
 };
 
 module.exports = Object.keys(aliases).reduce((map, alias) => {
