e4438e535d718654689cd4657ec36149cf7e6f8c
Cody Romano <codyromano@gmail.com>
2018-04-29 16:40:17 -0700

    Display treat resources

--- src/components/pages/TreatsPage/index.js
+++ src/components/pages/TreatsPage/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { CLASS_TREAT } from 'aurae-config/resourceClasses';
 import { PageWidthContainer } from 'aurae-components/layout';
+import { connect } from 'react-redux';
 
-export default class TreatsPage extends React.Component {
+class TreatsPage extends React.Component {
   render = () => (
     <PageWidthContainer>
-      Placeholder treats page
+      <ul>
+        {this.props.treats.map(treat => (
+          <li>{treat.title}</li>
+        ))}
+      </ul>
     </PageWidthContainer>
   )
 }
+
+const mapStateToProps = (state) => ({
+  treats: state.resources.byClass[CLASS_TREAT]
+    .map(id => state.resources.byId[id])
+});
+export default connect(mapStateToProps)(TreatsPage);
